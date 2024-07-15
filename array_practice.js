81005064ec77e53c6eb48f46a591bf1300926cf0
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-15 14:24:51 +0600

    last commit

---
+++ array_practice.js
@@ -0,0 +1,12 @@
+//তুমি এতদিন পর্যন্ত যে যে মডেলের মোবাইল ফোন ইউজ করেছো সেগুলার নাম দিয়ে একটা Array বানাও। তারপর একটা While লুপ দিয়ে সেই Array এর উপাদান গুলা একটা একটা করে আউটপুট হিসেবে দেখাও 
+
+var phoneArray = ['siemens', 'nokia xpress', 'nokia 5610', 'xiomi note 3', 'nokia 2.3', 'samsumg GT-10256', 'samsung galaxy duos', 'samsung galaxy F22']
+// var i = 3;
+
+console.log(phoneArray.indexOf('nokia 2.3'));
+
+// var i =0; //initializing the variable
+// while(i < phoneArray.length){
+//     console.log(phoneArray[i]);
+//     i++;
+// }
