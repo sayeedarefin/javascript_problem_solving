35d756f5c500482880e2d3ad9ec31bb7d59263be
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-15 15:29:06 +0600

    learnt avarage

---
+++ problem-3.js
@@ -0,0 +1,24 @@
+// Create an array with numerical values and use a for loop to calculate and print the average of the elements.
+
+var numbers = [2,4,6,8,10]
+sum = 0;
+
+
+for(i=0; i<numbers.length; i++){
+    sum += numbers[i];
+}
+var avg = sum/numbers.length;
+console.log(avg)
+
+
+
+
+
+// self practice
+var shonkha = [5,10,15,20,25,30,35,40];
+sum = 0;
+for(i=0; i<shonkha.length; i++){
+    sum += shonkha[i];
+}
+var avarage = sum/shonkha.length;
+console.log(avarage);
